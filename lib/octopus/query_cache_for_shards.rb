f31d888aa06fed4213d547c63376bef5bd978db5
Calos.chen <calos.chen@splashtop.com>
2024-06-05 11:05:36 +0800

    Fix the Passenger no query carch problem

--- lib/octopus/query_cache_for_shards.rb
+++ lib/octopus/query_cache_for_shards.rb
@@ -4,7 +4,10 @@ module Octopus
     module QueryCacheForShards
       %i(enable_query_cache! disable_query_cache!).each do |method|
         define_method(method) do
-          if(Octopus.enabled? && (shards = ActiveRecord::Base.connection.shards)['master'] == self)
+          if Octopus.enabled? && (
+            (shards = ActiveRecord::Base.connection.shards)['master'] == self ||
+            (defined?(PhusionPassenger) && self.db_config.name == 'primary')
+          )
             shards.each do |shard_name, v|
               if shard_name == 'master'
                 super()
